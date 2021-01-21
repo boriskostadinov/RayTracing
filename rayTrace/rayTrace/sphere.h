5f01a607dac803a1b5c6b59302094edde64ec30e
boriskostadinov <boriskostadinof@gmail.com>
2021-01-21 03:28:01 +0200

    Adding aabb + bvh

--- rayTrace/rayTrace/sphere.h
+++ rayTrace/rayTrace/sphere.h
@@ -4,18 +4,21 @@
 #include "vec3.h"
 
 class sphere : public hittable {
-public:
-    sphere() {}
-    sphere(point3 cen, double r, shared_ptr<material> m)
-        : center(cen), radius(r), mat_ptr(m) {};
+    public:
+        sphere() {}
+        sphere(point3 cen, double r, shared_ptr<material> m)
+            : center(cen), radius(r), mat_ptr(m) {};
 
-    virtual bool hit(
-        const ray& r, double t_min, double t_max, hit_record& rec) const override;
+        virtual bool hit(
+            const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
-public:
-    point3 center;
-    double radius;
-    shared_ptr<material> mat_ptr;
+        virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
+
+
+    public:
+        point3 center;
+        double radius;
+        shared_ptr<material> mat_ptr;
 };
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
@@ -42,5 +45,12 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     rec.set_face_normal(r, outward_normal);
     rec.mat_ptr = mat_ptr;
 
+    return true;
+}
+
+bool sphere::bounding_box(double time0, double time1, aabb& output_box) const {
+    output_box = aabb(
+        center - vec3(radius, radius, radius),
+        center + vec3(radius, radius, radius));
     return true;
 }
