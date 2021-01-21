9b4ad131dc375978e65406161547dd8909262774
boriskostadinov <boriskostadinof@gmail.com>
2021-01-22 01:31:17 +0200

    Adding Image Texture Mapping

--- rayTrace/rayTrace/main.cpp
+++ rayTrace/rayTrace/main.cpp
@@ -106,7 +106,6 @@ hittable_list random_scene() {
     return world;
 }
 
-
 hittable_list two_spheres() {
     hittable_list objects;
 
@@ -116,6 +115,14 @@ hittable_list two_spheres() {
     objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
 
     return objects;
+}
+
+hittable_list earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+    return hittable_list(globe);
 }
 
 
@@ -205,9 +212,16 @@ int main() {
         aperture = 0.1;
         break;
 
-    default:
     case 2:
         world = two_spheres();
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        break;
+
+    default:
+    case 4:
+        world = earth();
         lookfrom = point3(13, 2, 3);
         lookat = point3(0, 0, 0);
         vfov = 20.0;
